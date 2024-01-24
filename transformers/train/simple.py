e4af3822f649b626153c9e4215c8c15035b19a98
Akhil Bagaria <abagaria@hmc.edu>
2024-01-23 21:12:22 -0500

    improved hyperparameters for shakespere.

--- transformers/train/simple.py
+++ transformers/train/simple.py
@@ -1,21 +1,24 @@
 import pdb
 import torch
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
 from transformers.data.utils import CharDataLoader
 from transformers.model.decoder import TransformerDecoder
 
-batch_size = 32
-seq_len = 40
-embedding_size = 128
-n_blocks = 5
-n_heads = 10
-dropout_p = 0.1
-lr = 3e-4
+
+batch_size = 64
+seq_len = 256
+embedding_size = 384
+n_blocks = 6
+n_heads = 6
+dropout_p = 0.4
+lr = 1e-3
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-n_batches = 10000
+n_batches = 5_000
+plotting_interval = 10
 
 print(f'Using device: {device}')
 
@@ -33,7 +36,22 @@ model = TransformerDecoder(
 model = model.to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
-for i in range(n_batches):
+@torch.no_grad()
+def compute_validation_loss(model, loader):
+    val_x, val_y = loader.get_batch(split='val')
+    val_x = val_x.to(device)
+    val_y = val_y.to(device)
+    logits = model(val_x)
+    loss = F.cross_entropy(
+        logits.reshape(-1, loader.vocab_size),
+        val_y.reshape(-1))   
+    return loss.item()
+
+training_loss = []
+validation_loss = []
+
+
+for i in tqdm(range(n_batches), desc='Training'):
     x, y = loader.get_batch()
     x = x.to(device)
     y = y.to(device)
@@ -47,6 +65,14 @@ for i in range(n_batches):
     loss.backward()
     optimizer.step()
 
-    if i % 10 == 0:
-        print(loss.item())
+    if i % plotting_interval == 0:
+        val_loss = compute_validation_loss(model, loader)
+        training_loss.append(loss.item())
+        validation_loss.append(val_loss)
     
+        plt.plot(training_loss, label='Training Loss')
+        plt.plot(validation_loss, label='Validation Loss')
+        plt.grid()
+        plt.legend()
+        plt.savefig('loss.png')
+        plt.close()
